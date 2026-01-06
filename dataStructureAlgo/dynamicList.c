d7b30b1b402978fc8b541f787e5e54ad348644ce
rksvw <ritik.vw98@gmail.com>
2026-01-06 18:04:58 +0530

    dynamic list in c

---
+++ dataStructureAlgo/dynamicList.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int capacity = 3;
+    int *list = malloc(capacity * sizeof(int));
+
+    if (list == NULL)
+    {
+        return 1;
+    }
+
+    list[0] = 3;
+    list[1] = 5;
+    list[2] = 8;
+
+    int *temp = malloc(4 * sizeof(int));
+    if (temp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        temp[i] = list[i];
+    }
+
+    temp[3] = 10;
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n", temp[i]);
+    }
+
+    free(list);
+
+    return 0;
+}
