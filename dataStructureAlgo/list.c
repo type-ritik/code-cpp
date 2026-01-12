42ed02cac412b371a3b48190d3f62d0de09e43b4
rksvw <ritik.vw98@gmail.com>
2026-01-12 18:43:11 +0530

    refactor list delete_at method

--- dataStructureAlgo/list.c
+++ dataStructureAlgo/list.c
@@ -19,6 +19,12 @@ void add(int);
 // Insert at position
 void insert_at(int, int);
 
+// Delete at position
+void delete_at(int);
+
+// Delete item
+void delete_data();
+
 // Traverse in list
 void traverse();
 
@@ -26,22 +32,46 @@ int main()
 {
 
     head = NULL;
+    // insert_at(1, 1);
+    // insert_at(2, 2);
+    // // add(1);
+    // // add(2);
+    // add(3);
+    // // add(4);
+    // add(5);
+    // add(6);
+    // add(7);
+    // add(8);
+    // add(9);
+
+    // traverse();
+    // printf("Insertion at 4 index\n");
+    // insert_at(4, 4);
+    // traverse();
+    // insert_at(5, -5);
+
+    delete_at(1);
     insert_at(1, 1);
-    insert_at(2, 2);
-    // add(1);
-    // add(2);
-    add(3);
-    // add(4);
-    add(5);
-    add(6);
-    add(7);
-    add(8);
-    add(9);
+    traverse();
+    delete_at(1);
+    insert_at(1, 1);
+    traverse();
+    delete_at(2);
+    delete_at(1);
+    for (int i = 1; i < 11; i++)
+    {
+        if (i == 9)
+        {
+            insert_at(20, 9);
+        }
+        add(i);
+    }
 
     traverse();
-    printf("Insertion at 4 index\n");
-    insert_at(4, 4);
+    printf("Deletion at 9 index\n");
+    delete_at(9);
     traverse();
+    delete_at(20);
     return 0;
 }
 
@@ -71,12 +101,19 @@ void traverse()
 {
     for (Node *temp = head; temp != NULL; temp = temp->next)
     {
-        printf("%d\n", temp->data);
+        printf("%d->", temp->data);
     }
+    printf("NULL\n");
 }
 
 void insert_at(int item, int pos)
 {
+    if (pos < 1)
+    {
+        printf("Invalid position {%d}\n", pos);
+        return;
+    }
+
     if (head == NULL && pos != 1)
     {
         printf("List is NULL, can't insert at position {%d}\n", pos);
@@ -111,4 +148,51 @@ void insert_at(int item, int pos)
 
     node->next = curr->next;
     curr->next = node;
+}
+
+void delete_at(int pos)
+{
+    if (pos < 1)
+    {
+        printf("Invalid position {%d}\n", pos);
+        return;
+    }
+
+    if ((head == NULL) || (head->next == NULL && pos != 1))
+    {
+        printf("List is NULL, can't delete at position {%d}\n", pos);
+        return;
+    }
+
+    if (head->next == NULL && pos == 1)
+    {
+        free(head);
+        head = NULL;
+        return;
+    }
+
+    int count = 0;
+    Node *current = head;
+    Node *previous;
+
+    while (current->next != NULL && count != pos - 1)
+    {
+        if (count == pos - 2)
+        {
+            previous = current;
+        }
+
+        current = current->next;
+        count = count + 1;
+    }
+
+    if (count != pos - 1)
+    {
+        printf("List lenght is small\n");
+        return;
+    }
+
+    previous->next = current->next;
+    current->next = NULL;
+    free(current);
 }
