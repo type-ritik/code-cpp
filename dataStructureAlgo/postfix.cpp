834e3ebc464505f3955cf23beb8e92f3f899eca3
rksvw <ritik.vw98@gmail.com>
2026-02-02 08:19:41 +0530

    evaluate postfix refacter with edge cases

--- dataStructureAlgo/postfix.cpp
+++ dataStructureAlgo/postfix.cpp
@@ -44,11 +44,74 @@ int evaluatePostfix(string expression)
     return post.top();
 }
 
+int evalPostfix(string exp)
+{
+
+    if (!isdigit(exp[0]) || !isdigit(exp[1]))
+    {
+        cout << "Error: Expression must start with two operands." << endl;
+        return -1;
+    }
+    stack<char> stack;
+
+    for (char character : exp)
+    {
+        if (isdigit(character))
+        {
+            stack.push(character - '0');
+        }
+        else
+        {
+
+            int val2 = stack.top();
+            stack.pop();
+
+            if (stack.empty())
+            {
+                cout << "Error: Insufficient operands in the expression." << endl;
+                return -1;
+            }
+
+            int val1 = stack.top();
+            stack.pop();
+
+            switch (character)
+            {
+            case '+':
+                stack.push(val1 + val2);
+                break;
+            case '-':
+                stack.push(val1 - val2);
+                break;
+            case '/':
+                stack.push(val1 / val2);
+                break;
+            case '*':
+                stack.push(val1 * val2);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    int result = stack.top();
+    stack.pop();
+
+    if (!stack.empty())
+    {
+        cout << "Error: Stack is not empty after evaluation." << endl;
+        return -1;
+    }
+
+    return result;
+}
+
 int main(void)
 {
-    string expression = "34+5*";
+    string expression = "23*54*+9-";
     cout << "Postfix Expression: " << expression << endl;
     // cout << '5' - '0' << endl;
-    cout << "Evaluated Result: " << evaluatePostfix(expression) << endl;
+    cout << "Evaluated Result: " << evalPostfix(expression) << endl;
     return 0;
 }
