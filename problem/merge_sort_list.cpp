8f0b03f7e5294cc5288bc1afbbf43bc1042b59eb
rksvw <ritik.vw98@gmail.com>
2026-05-29 17:49:18 +0530

    leetcode linkedlist problem merge two sort list and remove duplicates from a list

--- problem/merge_sort_list.cpp
+++ problem/merge_sort_list.cpp
@@ -78,6 +78,93 @@ Node *mergeTwoList(Node *list1, Node *list2)
     return result;
 }
 
+Node *mergeTwoListIterativeApproach(Node *list1, Node *list2)
+{
+    Node *list = NULL;
+    Node *head = NULL;
+
+    if (list1 == NULL)
+    {
+        return list2;
+    }
+
+    if (list2 == NULL)
+    {
+        return list1;
+    }
+
+    int count = 0;
+
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->data <= list2->data)
+        {
+            if (head == NULL)
+            {
+                head = list1;
+                list = head;
+            }
+            else
+            {
+                list->next = list1;
+                list = list->next;
+            }
+            list1 = list1->next;
+        }
+        else
+        {
+            if (head == NULL)
+            {
+                head = list2;
+                list = head;
+            }
+            else
+            {
+                list->next = list2;
+                list = list->next;
+            }
+            list2 = list2->next;
+        }
+    }
+
+    if (list1 == NULL && list2 != NULL)
+    {
+        list->next = list2;
+    }
+    else
+    {
+        list->next = list1;
+    }
+
+    return head;
+}
+
+Node *removeDuplicates(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+
+    Node *curr = head;
+
+    while (curr->next != NULL)
+    {
+        if (curr->data == curr->next->data)
+        {
+            Node *duplicate = curr->next;
+            curr->next = curr->next->next;
+            delete duplicate;
+        }
+        else
+        {
+            curr = curr->next;
+        }
+    }
+
+    return head;
+}
+
 int main()
 {
     LinkedList ll;
@@ -93,9 +180,13 @@ int main()
     ll1.push_back(40);
 
     ll1.print_Node();
-    Node *merge_node = mergeTwoList(ll.head, ll1.head);
+    Node *merge_node = mergeTwoListIterativeApproach(ll.head, ll1.head);
 
     LinkedList merge_list;
     merge_list.head = merge_node;
     merge_list.print_Node();
+
+    LinkedList unique;
+    unique.head = removeDuplicates(merge_node);
+    unique.print_Node();
 }
